babed65e4a4148605fc247ed3e87dec76ecbbc02
Holly Henske <hcover333@gmail.com>
2021-06-08 10:26:11 -0500

    trouble with fetch and response

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // TODO: add code here
+//1
+window.addEventListener("load", function(){
+    //2
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then( function(json) {
+        //assuming html stuff and template go in the other file....here I need to set up the variables?
+            const container = document.getElementById("astronauts");
+            
+    })
+    })
+});
