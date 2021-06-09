e07d129a7135b206416cec8d7ec43833c836be47
Holly Henske <hcover333@gmail.com>
2021-06-09 18:11:07 -0500

    ready to turn in?

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ window.addEventListener("load", function(){
     //2
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then( function(json){
+            let container = document.getElementById("container");
             for (let i=0; i < json.length; i++) {
-            console.log(`${json[i].firstName} ${json[i].lastName}`);
-            //so the list of names appears in the console
-            //to do: get the first name in the first rectangle <h2> size
-            //to do: put a <ul> of other data inside each bos
-            //to do: make sure the loop repeats once, then creates a new rectangle
-            //To do: also in the rectangle, deal with the images
-            console.log(`Hours in Space: ${json[i].hoursinSpace}`);
-            console.log(`Active: ${json[i].active}`);
-            console.log(`Skills: ${json[i].skills}`);
+                container.innerHTML += `
+                <div class = "astronaut">
+                    <div class = "bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                    <div>
+                    <img class="avatar" src="${json[i].picture}">
+                    </div>`;
+
             }
         });
             
