4c4c47e96be222199538c95eb914886513201542
Holly Henske <hcover333@gmail.com>
2021-06-09 20:29:52 -0500

    finished!

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then( function(json){
             let container = document.getElementById("container");
+           
+
             for (let i=0; i < json.length; i++) {
                 container.innerHTML += `
                 <div class = "astronaut">
@@ -12,13 +14,19 @@ window.addEventListener("load", function(){
                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
                     <ul>
                         <li>Hours in Space: ${json[i].hoursInSpace}</li>
-                        <li>Active: ${json[i].active}</li>
+                        <li id = "active" color = black>Active: ${json[i].active}</li>
                         <li>Skills: ${json[i].skills}</li>
                     </ul>
                     <div>
                     <img class="avatar" src="${json[i].picture}">
                     </div>`;
+                  
+                   
+                     
+                        
+                       
 
+                 
             }
         });
             
